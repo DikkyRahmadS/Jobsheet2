a6d6cb34cd595375276053a1b81a086195b9522d
Cheeettoozzz <Cheeettoozzz@DESKTOP-6P23GR7>
2018-08-23 10:54:05 +0700

    Praktikum

---
+++ src/Biodata/Dikky.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Biodata;
+
+/**
+ *
+ * @author Cheeettoozzz
+ */
+public class Dikky {
+    public String namaAyah,namaIbu,namaSendiri,namaSaudara,Alamat,Hobi,CitaCita,Umur;
+    
+      public void setNamaAyah(String newNamaAyah){
+          namaAyah = newNamaAyah;
+           System.out.println("Nama Ayah : " + namaAyah) ;
+      }
+       public String getNamaAyah(){
+       return namaAyah;
+   }
+      public void setNamaIbu(String newNamaIbu){
+          namaIbu = newNamaIbu;
+           System.out.println("Nama Ibu : " + namaIbu) ;
+      }
+      public String getNamaIbu(){
+       return namaIbu;
+   }
+      public void setNamaSendiri(String newNamaSendiri){
+          namaSendiri = newNamaSendiri;
+           System.out.println("Nama Sendiri : " + namaSendiri) ;
+      }
+      public String getNamaSendiri(){
+       return namaSendiri;
+   }
+      public void setNamaSaudara(String newNamaSaudara){
+          namaSaudara = newNamaSaudara;
+           System.out.println("Nama Saudara : " + namaSaudara) ;
+      }
+      public String getNamaSaudara(){
+       return namaSaudara;
+   }
+      public void setAlamat(String newAlamat){
+          Alamat = newAlamat;
+           System.out.println("Alamat : " + Alamat) ;
+      }
+      public String getAlamat(){
+       return Alamat;
+   }
+      public void setHobi(String newHobi){
+          Hobi = newHobi;
+           System.out.println("Nama Hobi : " + Hobi) ;
+      }
+      public String getHobi(){
+       return Hobi;
+   }
+      public void setCitaCita(String newCitaCita){
+          CitaCita = newCitaCita;
+           System.out.println("Cita Cita : " + CitaCita) ;
+      }
+      public String getCitaCita(){
+       return CitaCita;
+   }
+      public void setUmur(String newUmur){
+          Umur = newUmur;
+           System.out.println("Umur : " + Umur) ;
+      }
+     public String getUmur(){
+       return Umur;
+   }
+
+}
---
+++ src/Biodata/Uji.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Biodata;
+
+/**
+ *
+ * @author Cheeettoozzz
+ */
+public class Uji {
+    public static void main(String[] args) {
+        Dikky test = new Dikky();
+        test.setNamaAyah("Julian");
+        test.setNamaIbu("Haebah");
+        test.setNamaSendiri("Dikky Rahmad Shafara");
+        test.setNamaSaudara("Diaz Rizqi ");
+        test.setAlamat(" Jalan Terusan Sigura gura ");
+        test.setHobi("Basket");
+        test.setCitaCita("Punya banyak lahan pekerjaan untuk orang lain");
+        test.setUmur("16");
+        
+    }
+    
+}
